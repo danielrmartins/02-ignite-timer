2dc1b669b365cc173f8451e2a8a9fde8defbdccd
Daniel <danielmartins10@gmail.com>
2024-09-04 16:29:22 -0300

    chore: add eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import { ThemeProvider } from "styled-components";
+import { ThemeProvider } from 'styled-components';
 
-import { Button } from "./components/Button"
-import { defaultTheme } from "./styles/themes/default";
-import { GlobalStyle } from "./styles/global";
+import { Button } from './components/Button';
+import { defaultTheme } from './styles/themes/default';
+import { GlobalStyle } from './styles/global';
 
 export function App() {
   return (
@@ -12,10 +12,10 @@ export function App() {
       <Button variant="success" />
       <Button variant="danger" />
       <Button />
-      
+
       <GlobalStyle />
     </ThemeProvider>
-  )
+  );
 }
 
-export default App
+export default App;
