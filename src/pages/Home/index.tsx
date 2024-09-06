0040e057e48c38d4155fe95f4e1ab502196e9e9d
Daniel <danielmartins10@gmail.com>
2024-09-06 09:57:33 -0300

    chore: change inputs

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,14 @@
 import { Play } from 'phosphor-react';
 
-import { CountdownContainer, FormContainer, HomeContainer, Separator } from './styles';
+import {
+  CountdownContainer,
+  FormContainer,
+  HomeContainer,
+  MinutesAmountInput,
+  Separator,
+  StartCountdownButton,
+  TaskInput,
+} from './styles';
 
 export function Home() {
   return (
@@ -8,10 +16,17 @@ export function Home() {
       <form>
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
-          <input id="task" />
+          <TaskInput id="task" list="task-suggestions" placeholder="Dê um nome para o seu projeto" />
+
+          <datalist id="task-suggestions">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+            <option value="Banana" />
+          </datalist>
 
           <label htmlFor="minutesAmount">durante</label>
-          <input type="number" id="minutesAmount" />
+          <MinutesAmountInput type="number" id="minutesAmount" placeholder="00" step={5} min={5} max={60} />
 
           <span>minutos.</span>
         </FormContainer>
@@ -24,10 +39,10 @@ export function Home() {
           <span>0</span>
         </CountdownContainer>
 
-        <button type="submit">
+        <StartCountdownButton disabled type="submit">
           <Play size={24} />
           Começar
-        </button>
+        </StartCountdownButton>
       </form>
     </HomeContainer>
   );
